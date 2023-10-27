e23e73a8abb6e17fb1e45ee62ec69d29c5a81d87
Brian <47805062+Ambiivalent@users.noreply.github.com>
2023-10-26 21:05:43 -0400

    Update exercise_api.py
    change app instance and added db connection (temp)

--- database/exercise_api.py
+++ database/exercise_api.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
+from flask import Blueprint
 import mysql.connector
+from mysql.connector import errorcode
 
-app = Flask(__name__)
+exercise_api = Blueprint('exercise_api', __name__)
 
-@app.route('/add_exercise', methods=['POST'])
-def add_exercise(db):
+@exercise_api.route('/add_exercise', methods=['POST'])
+def add_exercise():
     try:
         repetitions = request.json['repetitions']
         numSets = request.json['numsets']
         exerciseName = request.json['exercisename']
-        
+        db = connect_to_database()
+
         cursor = db.cursor()
         
         query = "INSERT INTO ExerciseLog (Repetitions, NumSets, ExerciseName) VALUES (%s, %s, %s)"
@@ -17,15 +20,16 @@ def add_exercise(db):
         cursor.execute(query, values)
         cursor.close()
         
-        return jsonify({"message": "Exercise added successfully."}), 200
+        return jsonify({"message": "Exercise added successfully."}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
-@app.route('/get_exercise', methods=['GET'])
-def get_exercise(db):
+@exercise_api.route('/get_exercise', methods=['GET'])
+def get_exercise():
     try:
         exercise_id = request.json['exerciseid']
-        
+        db = connect_to_database()
+
         cursor = db.cursor()
         
         query = "SELECT * WHERE ExerciseID = %s"
@@ -34,10 +38,26 @@ def get_exercise(db):
         data = cursor.fetchone()
         cursor.close()
 
-        return jsonify.dumps(data)
+        return jsonify.dumps(data), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400        
         
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def connect_to_database():
+    data = {
+        # to be changed -> read from file later or preconnect to db.
+    "user": "root",
+    "password": "Cps714password",
+    "host": "127.0.0.1",
+    "database": "Fitness"
+    }
+    try:
+        cnx = mysql.connector.connect(**data)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    
+    return cnx
