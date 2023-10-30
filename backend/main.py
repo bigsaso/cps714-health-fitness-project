5e7e3e8dad4a5761ecd11d19067fc609f9c34628
sukhroop1214 <143036623+sukhroop1214@users.noreply.github.com>
2023-10-30 19:51:53 -0400

    Added Sleep Data API

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,13 @@ from flask import Flask
 from blueprints.basic_endpoints import blueprint as basic_endpoints
 from blueprints.jinja_endpoint import blueprint as jinja_template_blueprint
 from blueprints.login_endpoints import blueprint as login
+from blueprints.sleep_data_api import blueprint as sleep_data_api
 
 app = Flask(__name__)
 app.register_blueprint(basic_endpoints)
 app.register_blueprint(jinja_template_blueprint)
 app.register_blueprint(login)
+app.register_blueprint(sleep_data_api)
 
 if __name__ == "__main__":
     app.run()
