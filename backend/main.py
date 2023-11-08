fdae44a993aa4a53ca99874bd416c4be1b3b8651
JulianMiceli <julian.miceli2001@gmail.com>
2023-11-08 16:41:26 -0500

    Modified mood_api to reflect database changes, created goals_api and user_api and tested all changes.

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,8 @@ from blueprints.exercise_api import exercise_api as exercise_api
 from blueprints.mood_api import blueprint as mood_api
 from blueprints.calorie_api import blueprint as calorie_api
 from blueprints.create_account_api import blueprint as create_account_api
-
-
-
+from blueprints.goals_api import blueprint as goals_api
+from blueprints.user_api import blueprint as user_api
 
 app = Flask(__name__)
 CORS(app)
@@ -26,6 +25,8 @@ app.register_blueprint(exercise_api)
 app.register_blueprint(mood_api)
 app.register_blueprint(calorie_api)
 app.register_blueprint(create_account_api)
+app.register_blueprint(goals_api)
+app.register_blueprint(user_api)
 
 if __name__ == "__main__":
     app.run()
